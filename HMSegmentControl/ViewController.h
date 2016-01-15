bfc36c0bb5a523a7b7b00ad1529aa6966adf5120
leiliang <1007279149@qq.com>
2016-01-15 14:12:53 +0800

    Initial Commit

---
+++ HMSegmentControl/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HMSegmentControl
+//
+//  Created by mac on 16/1/15.
+//  Copyright © 2016年 leiliang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
